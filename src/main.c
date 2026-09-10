5bf82e93c3b9dfcfd03c6310b44faf2aff9bead5
Sebastian2852 <sebastian2852@proton.me>
2026-09-10 01:23:23 +0100

    Remove duplicate code

--- src/main.c
+++ src/main.c
@@ -55,44 +55,38 @@ int main() {
     StringView hostStr = {0};
     StringView pathStr = {0};
 
+    size_t stripLen = 0;
     if (svStartsWith(&sv, "git@")) {
-        svStrip(&sv, 4, SV_STRIP_FRONT);
-        // This is hardcoded for a github.com length hostname.
-        bool didSub = svSubStr(&sv, &hostStr, 0, 10);
-        if (!didSub) {
-            fprintf(stderr, "[ERROR] Failed to substr the hostname");
-            return 1;
-        }
-
-        didSub = svSubStr(&sv, &pathStr, 11, sv.length);
-        if (!didSub) {
-            fprintf(stderr, "[ERROR] Failed to substr the path");
-            return 1;
-        }
-
-        repo.host = hostStr.characters;
-        repo.path = pathStr.characters;
+        printf("[INFO] Uses SSH");
+        stripLen = 4;
     } else if (svStartsWith(&sv, "https://")) {
-        svStrip(&sv, 8, SV_STRIP_FRONT);
-        // This is hardcoded for a github.com length hostname.
-        bool didSub = svSubStr(&sv, &hostStr, 0, 10);
-        if (!didSub) {
-            fprintf(stderr, "[ERROR] Failed to substr the hostname");
-            return 1;
-        }
-
-        didSub = svSubStr(&sv, &pathStr, 11, sv.length);
-        if (!didSub) {
-            fprintf(stderr, "[ERROR] Failed to substr the path");
-            return 1;
-        }
-
-        repo.host = hostStr.characters;
-        repo.path = pathStr.characters;
+        printf("[INFO] Uses https");
+        stripLen = 8;
+    } else {
+        fprintf(stderr, "[ERROR] Unsupported format.\n");
+        return 1;
     }
 
+    svStrip(&sv, stripLen, SV_STRIP_FRONT);
+
+    // This is hardcoded for a github.com length hostname.
+    bool didSub = svSubStr(&sv, &hostStr, 0, 10);
+    if (!didSub) {
+        fprintf(stderr, "[ERROR] Failed to substr the hostname");
+        return 1;
+    }
+
+    didSub = svSubStr(&sv, &pathStr, 11, sv.length);
+    if (!didSub) {
+        fprintf(stderr, "[ERROR] Failed to substr the path");
+        return 1;
+    }
+
+    repo.host = hostStr.characters;
+    repo.path = pathStr.characters;
+
     if (repo.host == NULL || repo.path == NULL) {
-        fprintf(stderr, "[ERROR] Unsupported format.\n");
+        fprintf(stderr, "[ERROR] Failed to extract host or path");
         return 1;
     }
 
